1a3bf516d74983153d12a9ec0c32be1f89a1c1f9
huicando <44965363+huicando@users.noreply.github.com>
2019-10-23 12:02:22 +0800

    修改后的版本
    直接执行就可以了

--- undefined.sql
+++ undefined.sql
@@ -11,11 +11,31 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 22/10/2019 22:12:14
+ Date: 23/10/2019 12:00:33
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`  (
+  `orderid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `shopid` int(100) NOT NULL COMMENT '商品id',
+  `shopname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '商品名',
+  `buyeropenid` varchar(160) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '买家id',
+  `buyername` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '买家名字',
+  `selleropenid` varchar(160) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '卖家id',
+  `sellername` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '卖家名字',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '卖家地址',
+  `phone` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '卖家联系电话',
+  `time` datetime(0) NOT NULL COMMENT '拍下时间',
+  `comments` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '留言',
+  `flag` tinyint(4) UNSIGNED ZEROFILL NOT NULL COMMENT '是否同意卖',
+  PRIMARY KEY (`orderid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for shop
@@ -30,12 +50,11 @@ CREATE TABLE `shop`  (
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Transaction` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '交易方式 ',
   `phone` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `cost` float(10, 2) NULL DEFAULT NULL,
+  `cost` float(10, 2) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '物品名',
   `kinds` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `amount` int(10) NOT NULL,
   `time` datetime(0) NOT NULL COMMENT '发布时间',
-  `shock-time` datetime(0) NULL DEFAULT NULL COMMENT '被锁的时间',
   `pictures` json NOT NULL,
   `view` int(30) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -55,6 +74,7 @@ CREATE TABLE `userinfo`  (
   `avatarUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '头像连接',
   `gender` varchar(4) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
   `phone` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话',
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '宿舍号',
   PRIMARY KEY (`openid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
